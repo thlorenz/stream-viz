f78a354de6996a763f442be78c9c1143c0d13cbd
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-27 08:38:37 -0400

    showing read buffer gauge

--- spikes/gauge/main.js
+++ spikes/gauge/main.js
@@ -1,9 +1,27 @@
 'use strict';
 
-var numbers = require('../streams/number-readable')
-  , sviz = require('../../')
-  , el = document.getElementById('numbers')
+var numbers  =  require('../streams/number-readable')
+  , sviz     =  require('../../')
+  , nebraska =  require('nebraska')
+  , through  =  require('through2')
+  , el       =  document.getElementById('numbers')
 
-var nums = numbers({ to: 500, throttle: 200 });
-nums
-  .pipe(sviz('gauge', el, { label: 'r-buffer', max: 500, clazz: 'simple' }))
+var nums       =  numbers({ to: 500, throttle: 200 });
+var numsState  =  nebraska(nums, { interval: 400 })
+var rbufferLen =  numsState.pipe(through({ objectMode: true }, pluckReadableBufferLen))
+//var wbufferLen =  numsState.pipe(through({ objectMode: true }, pluckWritableBufferLen))
+
+function pluckReadableBufferLen (chunk, encoding, cb) {
+  console.log(chunk.readable.bufferLength);
+  this.push(chunk.readable.bufferLength);
+  cb()
+}
+
+function pluckWritableBufferLen (chunk, encoding, cb) {
+  this.push(chunk.writable.bufferLength);
+  cb()
+}
+
+nums.pipe(sviz('gauge', el, { label: 'nums', max: 500, clazz: 'simple' }))
+
+rbufferLen.pipe(sviz('gauge', el, { label: 'r-buffer', max: 16, clazz: 'simple' }, true))
--- streams-viz.js
+++ streams-viz.js
@@ -2,9 +2,9 @@
 
 var d3gauge = require('d3-gauge-writable');
 
-var go = module.exports = function (viztype, el, opts) {
+var go = module.exports = function (viztype, el, opts, objectMode) {
   switch (viztype) {
-    case 'gauge': return d3gauge(el, { gauge: opts });
+    case 'gauge': return d3gauge(el, { gauge: opts, objectMode: objectMode });
     default: throw new Error('Unkown viztype: ' + viztype);
   }
 };
