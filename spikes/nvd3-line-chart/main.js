e295b9593b2ddcae015363c85285c128f26120ab
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-28 19:29:33 -0400

    consolidating streaming nvd3 chart spike

--- spikes/nvd3-line-chart/main.js
+++ spikes/nvd3-line-chart/main.js
@@ -1,74 +1,68 @@
 'use strict';
-
-var d3 = require('d3');
 var nv = require('nvd3');
+var d3 = require('d3');
 
-function sinAndCos() {
-  var sin =  [],
-    cos   =  [],
-    rand  =  [],
-    rand2 =  []
-    ;
+var chart;
+var data = [{
+	key: "Stream 1",
+	color: "orange",
+	values: [
+		{x: 1, y: 1}
+	]
+}];
 
-  for (var i = 0; i < 100; i++) {
-    sin.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) }); //the nulls are to show how defined works
-    cos.push({x: i, y: 0.5 * Math.cos(i/10)});
-    rand.push({x:i, y: Math.random() / 10});
-    rand2.push({x: i, y: Math.cos(i/10) + Math.random() / 10 })
-  }
+nv.addGraph(function() {
+  
+  chart = nv.models.historicalBarChart();
 
-  return [
-    { area: true,
-      values: sin,
-      key: "Sine Wave",
-      color: "#ff7f0e"
-    },
-    { values: cos,
-      key: "Cosine Wave",
-      color: "#2ca02c"
-    },
-    { values: rand,
-      key: "Random Points",
-      color: "#2222ff"
-    } ,
-    { values: rand2,
-      key: "Random Cosine",
-      color: "#667711"
-    }
-  ];
-}
+  chart.x(function(d, i) { return d.x });
+      
+  // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return
+  // themselves, not the parent chart, so need to chain separately
+  chart.xAxis 
+    .tickFormat(d3.format(',.1f'))
+    .axisLabel("Time")
 
-function createGraph (id) {
-  var chart;
+  chart.yAxis
+    .axisLabel('Random Number')
+    .tickFormat(d3.format(',.4f'));
 
-  nv.addGraph(function() {
-      chart = nv.models.lineChart()
-      .options({
-        margin: { left: 100, bottom: 100},
-        x: function(d,i) { return i},
-        showXAxis: true,
-        showYAxis: true,
-        transitionDuration: 250
-      })
+  chart
+    .showXAxis(true)
+    .showYAxis(true)
+    .rightAlignYAxis(true)
+    .margin({ right: 90 });
 
-    // chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the parent chart, so need to chain separately
-    chart.xAxis
-      .axisLabel("Time (s)")
-      .tickFormat(d3.format(',.1f'));
-
-    chart.yAxis
-      .axisLabel('Voltage (v)')
-      .tickFormat(d3.format(',.2f'))
-      ;
-
-    d3.select('#' + id + ' svg')
-      .datum(sinAndCos())
+  d3.select('#chart svg')
+      .datum(data)
+      .transition()
+        .duration(1)
       .call(chart);
 
-    chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+  nv.utils.windowResize(chart.update);
 
-    return chart;
-  })
-}
+  return chart;
+});
 
-createGraph('line-chart');
+var x = 2;
+var run = true;
+setInterval(function(){
+	if (!run) return;
+	
+	var spike = (Math.random() > 0.95) ? 10: 1;
+	data[0].values.push({
+		x: x,
+		y: Math.random() * spike
+	});
+
+	if (data[0].values.length > 70) {
+		data[0].values.shift();
+	}
+	x++;
+
+	chart.update();
+}, 100);
+
+d3.select("#start-stop-button").on("click",function() {
+	run = !run;
+});
