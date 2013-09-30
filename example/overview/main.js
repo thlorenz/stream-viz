59723511a62cef7168119a538bd257c0a36416cc
Thorsten Lorenz <thlorenz@gmx.de>
2013-09-30 19:32:13 -0400

    first step in pulling parts out and creating simplified sviz function

--- example/overview/main.js
+++ example/overview/main.js
@@ -8,93 +8,44 @@ var numbers   =  require('../streams/number-readable')
   , nebraska  =  require('nebraska')
   , chunkRate =  require('chunk-rate-readable')
 
-// transforms
-var pluckWritableBufferLen =  require('../../lib/transforms/pluck-writable-buffer-len-transform')
-  , pluckReadableBufferLen =  require('../../lib/transforms/pluck-readable-buffer-len-transform')
-  , pluckWritableState     =  require('../../lib/transforms/pluck-writable-state-transform')
-  , pluckReadableState     =  require('../../lib/transforms/pluck-readable-state-transform')
-  , logger                 =  require('../../lib/transforms/logger-transform') 
-
 // DOM elements
 var numsEl   =  document.getElementById('numbers')
   , powersEl =  document.getElementById('powers')
   , tarpitEl =  document.getElementById('tarpit')
   
-var readableProps = [
-    'highWaterMark'
-  , 'bufferLength'
-  , 'pipesCount'
-  , 'flowing'
-  , 'reading' 
-  , 'objectMode'
-]
-
-var writableProps = [
-    'highWaterMark'
-  , 'bufferLength'
-  , 'objectMode'
-  , 'needDrain'
-  , 'writing'
-  , 'bufferProcessing'
-  , 'writelen'
-]
-
-var objectMode = true
-
-function vizOverview (rootEl, stream) {
-  var rootRow1      =  rootEl.getElementsByClassName('row1')[0]
-    , rootRow2      =  rootEl.getElementsByClassName('row2')[0]
-    , rootRow3      =  rootEl.getElementsByClassName('row3')[0]
-    , readableState =  stream._readableState
-    , writableState =  stream._writableState
-
-  var streamRate   =  chunkRate(stream, { interval: 2000 })
-    , streamState  =  nebraska(stream, { 
-          interval: 400
-        , readable: readableProps 
-        , writable: writableProps 
-      })
-
-  if (writableState) {
-    streamState
-      .pipe(pluckWritableBufferLen())
-      .pipe(sviz.gauge(rootRow2, { label: 'writable', max: writableState.highWaterMark, size: 150 }, true))
-
-    streamState
-      .pipe(pluckWritableState())
-      .pipe(sviz.tabject(rootRow3, { tabject: { label: 'Writable State' } }))
-  }
-
-  if (readableState) {
-    stream
-      .pipe(sviz.ticker(rootRow1, objectMode))
-
-    streamRate
-      .pipe(sviz.lineChart(rootRow1))
-
-    streamState
-      .pipe(pluckReadableBufferLen())
-      .pipe(sviz.gauge(rootRow2, { label: 'readable', max: readableState.highWaterMark, size: 150 }, true))
-
-    streamState
-      .pipe(pluckReadableState())
-      .pipe(sviz.tabject(rootRow3, { tabject: { label: 'Readable State' } }))
-  }
-
-}
-
+var objectMode = true;
 var nums   =  numbers({ objectMode: objectMode, throttle: 200,  highWaterMark: 20 , to: 5000})
   , powers =  powers( { objectMode: objectMode, throttle: 1000, highWaterMark: 20 })
   , pit    =  tarpit( { objectMode: objectMode, throttle: 2000, highWaterMark: 40 })
 
-vizOverview(numsEl, nums)
-vizOverview(powersEl, powers)
-vizOverview(tarpitEl, pit)
+function getRows (rootEl) {
+  var row1 =  rootEl.getElementsByClassName('row1')[0]
+    , row2 =  rootEl.getElementsByClassName('row2')[0]
+    , row3 =  rootEl.getElementsByClassName('row3')[0]
 
+  return [ null, row1, row2, row3 ];
+}
+
+function getOpts(rows) {
+  return {
+      rate: { element: rows[1] }
+    , writableGauge: { element: rows[2] } 
+    , readableGauge: { element: rows[2] } 
+    , writableState: { element: rows[3] } 
+    , readableState: { element: rows[3] } 
+  }
+}
+
+var numsOpts = getOpts(getRows(numsEl))
+  , powersOpts = getOpts(getRows(powersEl))
+  , tarpitOpts = getOpts(getRows(tarpitEl))
+
+sviz(nums, numsOpts)
+sviz(powers, powersOpts)
+sviz(pit, tarpitOpts)
 
 addThrottleRange(numsEl, nums)
 addThrottleRange(powersEl, powers)
 addThrottleRange(tarpitEl, pit)
 
 nums.pipe(powers).pipe(pit)
-
